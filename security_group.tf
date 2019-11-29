380fc246ed382af723b0dbf0c77a17bb9f3656b6
yukin01 <bump1105@gmail.com>
2019-11-29 16:36:11 +0900

    Add security groups

---
+++ security_group.tf
@@ -0,0 +1,54 @@
+resource "aws_security_group" "lb" {
+  name        = "lb"
+  description = "security group for loadbalancer"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "lb"
+  }
+}
+
+resource "aws_security_group" "api" {
+  name        = "api"
+  description = "security group for api"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.lb.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "api"
+  }
+}
