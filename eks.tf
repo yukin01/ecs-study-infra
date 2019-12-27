2f841edb23fcb71701671b56446e984682fe2bbc
yukin01 <bump1105@gmail.com>
2019-12-27 18:12:24 +0900

    Add eks cluster

--- eks.tf
+++ eks.tf
@@ -1,16 +1,46 @@
-resource "aws_eks_cluster" "main" {
-  name = "kinjo-cluster"
-  role_arn = ""
+# resource "aws_eks_cluster" "main" {
+#   name = "${var.prefix}-cluster"
+#   role_arn = aws_iam_role.eks_cluster.arn
 
-  vpc_config {
-    subnet_ids = [
-      aws_subnet.private["private-a"].id,
-      aws_subnet.private["private-c"].id
-      # aws_subnet.public["public-a"].id,
-      # aws_subnet.public["public-c"].id
-    ]
-    security_group_ids = [
-      aws_security_group.api.id
-    ]
+#   vpc_config {
+#     subnet_ids = [
+#       aws_subnet.private["private-a"].id,
+#       aws_subnet.private["private-c"].id,
+#       aws_subnet.public["public-a"].id,
+#       aws_subnet.public["public-c"].id
+#     ]
+#     security_group_ids = [
+#       aws_security_group.api.id
+#     ]
+#   }
+
+#   depends_on = [
+#     aws_iam_role_policy_attachment.eks_cluster_policy,
+#     aws_iam_role_policy_attachment.eks_service_policy
+#   ]
+# }
+
+data "aws_iam_policy_document" "eks_cluster" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
   }
+}
+
+resource "aws_iam_role" "eks_cluster" {
+  name = "${var.prefix}-eks-cluster-role"
+  assume_role_policy = data.aws_iam_policy_document.eks_cluster.json
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_service_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks_cluster.name
 }
--- lb.tf
+++ lb.tf
@@ -1,48 +1,48 @@
-resource "aws_lb" "main" {
-  name                       = "fargate-study-lb"
-  load_balancer_type         = "application"
-  internal                   = false
-  idle_timeout               = 60
-  enable_deletion_protection = false
+# resource "aws_lb" "main" {
+#   name                       = "fargate-study-lb"
+#   load_balancer_type         = "application"
+#   internal                   = false
+#   idle_timeout               = 60
+#   enable_deletion_protection = false
 
-  subnets = [
-    aws_subnet.public["public-a"].id,
-    aws_subnet.public["public-c"].id
-  ]
+#   subnets = [
+#     aws_subnet.public["public-a"].id,
+#     aws_subnet.public["public-c"].id
+#   ]
 
-  security_groups = [
-    aws_security_group.lb.id
-  ]
-}
+#   security_groups = [
+#     aws_security_group.lb.id
+#   ]
+# }
 
-resource "aws_lb_target_group" "main" {
-  name        = "fargate-study-tg"
-  port        = 80
-  protocol    = "HTTP"
-  target_type = "ip"
-  vpc_id      = aws_vpc.main.id
+# resource "aws_lb_target_group" "main" {
+#   name        = "fargate-study-tg"
+#   port        = 80
+#   protocol    = "HTTP"
+#   target_type = "ip"
+#   vpc_id      = aws_vpc.main.id
 
-  health_check {
-    path                = "/"
-    healthy_threshold   = 5
-    unhealthy_threshold = 2
-    timeout             = 5
-    interval            = 30
-    matcher             = 200
-    port                = "traffic-port"
-    protocol            = "HTTP"
-  }
+#   health_check {
+#     path                = "/"
+#     healthy_threshold   = 5
+#     unhealthy_threshold = 2
+#     timeout             = 5
+#     interval            = 30
+#     matcher             = 200
+#     port                = "traffic-port"
+#     protocol            = "HTTP"
+#   }
 
-  depends_on = [aws_lb.main]
-}
+#   depends_on = [aws_lb.main]
+# }
 
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.main.arn
-  port              = 80
-  protocol          = "HTTP"
+# resource "aws_lb_listener" "http" {
+#   load_balancer_arn = aws_lb.main.arn
+#   port              = 80
+#   protocol          = "HTTP"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.main.arn
-  }
-}
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.main.arn
+#   }
+# }
